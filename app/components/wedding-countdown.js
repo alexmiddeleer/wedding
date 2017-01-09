97b03029acac4815faceb3801eb6b1596958fe46
Alex <almiddeleer@gmail.com>
2017-01-08 21:55:52 -0500

    Add basic wedding countdown

---
+++ app/components/wedding-countdown.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import { task } from 'ember-concurrency';
+
+const { Component, set, get } = Ember;
+
+const dateOfWedding = new Date('2017-10-28:17:00');
+
+export default Component.extend({
+  tagName: 'span',
+  startCountdown: task(function*() {
+    let dateNow = new Date();
+    let days = (dateOfWedding - dateNow) / 1000 / 60 / 60 / 24;
+    let hours = (days % 1) * 24;
+    let minutes = (hours % 1) * 60;
+    let seconds = (minutes % 1) * 60;
+    yield set(this, 'daysLeft', Math.floor(days));
+    yield set(this, 'hoursLeft', Math.floor(hours));
+    yield set(this, 'minutesLeft', Math.floor(minutes));
+    yield set(this, 'secondsLeft', Math.floor(seconds));
+    yield setTimeout(() => get(this, 'startCountdown').perform(), 1000);
+  }),
+
+  didInsertElement() {
+    get(this, 'startCountdown').perform();
+  }
+});
