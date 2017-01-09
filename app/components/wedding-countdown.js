be37d540eaf45bf0a92ecd49622f99d87100eb2f
Alex <almiddeleer@gmail.com>
2017-01-08 22:07:00 -0500

    Some final tweaks to the countdown

--- app/components/wedding-countdown.js
+++ app/components/wedding-countdown.js
@@ -9,15 +9,22 @@ export default Component.extend({
   tagName: 'span',
   startCountdown: task(function*() {
     let dateNow = new Date();
-    let days = (dateOfWedding - dateNow) / 1000 / 60 / 60 / 24;
-    let hours = (days % 1) * 24;
-    let minutes = (hours % 1) * 60;
-    let seconds = (minutes % 1) * 60;
-    yield set(this, 'daysLeft', Math.floor(days));
-    yield set(this, 'hoursLeft', Math.floor(hours));
-    yield set(this, 'minutesLeft', Math.floor(minutes));
-    yield set(this, 'secondsLeft', Math.floor(seconds));
-    yield setTimeout(() => get(this, 'startCountdown').perform(), 1000);
+    if (dateNow > dateOfWedding) {
+      yield set(this, 'daysLeft', 0);
+      yield set(this, 'hoursLeft', 0);
+      yield set(this, 'minutesLeft', 0);
+      yield set(this, 'secondsLeft', 0);
+    } else {
+      let days = (dateOfWedding - dateNow) / 1000 / 60 / 60 / 24;
+      let hours = (days % 1) * 24;
+      let minutes = (hours % 1) * 60;
+      let seconds = (minutes % 1) * 60;
+      yield set(this, 'daysLeft', Math.floor(days));
+      yield set(this, 'hoursLeft', Math.floor(hours));
+      yield set(this, 'minutesLeft', Math.floor(minutes));
+      yield set(this, 'secondsLeft', Math.floor(seconds));
+      yield setTimeout(() => get(this, 'startCountdown').perform(), 1000);
+    }
   }),
 
   didInsertElement() {
